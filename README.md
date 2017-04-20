60da1c71c4e9513d3a72340864598d9f15896a30
Rohit Ahuja <rahuja95@gmail.com>
2017-04-20 13:44:02 -0400

    finished bot template

---
+++ README.md
@@ -0,0 +1,60 @@
+# Django Messenger Bot Template
+
+A project starter template for Django 1.11 that is production ready for creating a Facebook Messenger bot deployed to Heroku.
+
+## Features
+
+- Production-ready configuration for Static Files, Database Settings, Gunicorn, etc.
+- Enhancements to Django's static file serving functionality via WhiteNoise.
+- Base application for developing a Facebook Messenger bot with all the helpful utilities.
+- Uses Python 2.7.
+
+## How to Use
+
+To use this project, follow these steps:
+
+### Seting up your Environment
+
+Install Django:
+
+    $ pip install django
+
+### Creating your Project
+
+Using this template to create a new Django app is easy:
+
+    $ django-admin.py startproject --template=https://github.com/rohitahuja/django-bytesize-template/archive/master.zip --name=Procfile helloworld
+
+You can replace ``helloworld`` with your desired project name.
+
+### Setting up the Project
+
+Enter your project folder:
+
+    $ cd helloworld
+
+Create your virtual environment and activate it:
+
+    $ virtualenv venv
+    $ source ./venv/bin/activate
+
+### Deployment to Heroku
+
+Initialize version control for the project:
+
+    $ git init
+    $ git add -A
+    $ git commit -m "Initial commit"
+
+Create a Heroku project:
+
+    $ heroku create
+    $ git push heroku master
+
+### Connecting to a Messenger Bot
+
+Create your messenger bot on https://developers.facebook.com/ 
+
+Add the products Messenger and Webhooks. On the Webhooks tab, create a New Subscription with the Callback URL as `http://<your-heroku-subdomain>.herokuapp.com/webhook/` and the Verify Token as ``johnharvard``. For Fields, check ONLY the one with ``message``.
+
+On the Messenger tab, under "Webhooks", subscribe the webhook we just created to the page you want to connect your bot to. Also, under "Token Generation", generate a token for the page we're connecting to and replace the variable ``page_access_token`` at the top of ``message.py`` with it.
---
+++ bot/message.py
@@ -0,0 +1,99 @@
+import requests
+
+# This is the token that you generate for the page hooked up to your bot
+page_access_token = "<REPLACE WITH YOUR PAGE ACCESS TOKEN!!!!>"
+
+# Messenger request URL
+url = "https://graph.facebook.com/v2.8/me/messages?access_token=" + page_access_token
+
+"""
+    Messenger sends us batches of messages. Thus, the payload that we receive
+    will have the following structure:
+
+    {
+      "object":"page",
+      "entry":[
+        {
+          "id":"PAGE_ID",
+          "time":1458692752478,
+          "messaging":[
+            {
+              "sender":{
+                "id":"USER_ID"
+              },
+              "recipient":{
+                "id":"PAGE_ID"
+              },
+              "timestamp":1458692752478,
+              "message":{
+                "mid":"mid.1457764197618:41d102a3e1ae206a38",
+                "text":"hello, world!",
+              }
+            }
+            ...
+          ]
+        }
+      ]
+    }
+
+    We see that "entry" is an array containing event data (interactions
+        that our bot experiences).
+    Each entry contains a "messaging" field, which is an array of
+        objects related to messaging. In this bot, however, we will only
+        handle receiving text messages. Thus, each "messaging" object will have
+        a "text" field.
+"""
+
+
+def handle_entries(payload):
+    """handle_entries
+
+    This is the main handling function for responding to message entries.
+
+    Ref: https://developers.facebook.com/docs/messenger-platform/webhook-reference
+    """
+    pass
+
+
+def handle_message(message):
+    """handle_message
+
+    This function will handle and respond to messages.
+
+    Note that the 'message' parameter will have the following structure:
+        {
+          "sender":{
+            "id":"USER_ID"
+          },
+          "recipient":{
+            "id":"PAGE_ID"
+          },
+          "timestamp":1458692752478,
+          "message":{
+            "mid":"mid.1457764197618:41d102a3e1ae206a38",
+            "text":"hello, world!",
+          }
+        }
+    """
+    pass
+
+
+def send_message(text, recipient):
+    """send_message
+
+    This function will send a text message to the recipient.
+
+    Messenger expects the following format:
+
+    {
+      "recipient":{
+        "id":"USER_ID"
+      },
+      "message":{
+        "text":"hello, world!"
+      }
+    }
+
+    Ref: https://developers.facebook.com/docs/messenger-platform/send-api-reference
+    """
+    pass
